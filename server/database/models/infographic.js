14d5b4db04a97b37873c2e69fdd2aca408fd1ca2
David Jelic <david@uxpassion.com>
2016-12-06 17:08:57 +0100

    [model/infographic] Add uri attribute

--- server/database/models/infographic.js
+++ server/database/models/infographic.js
@@ -9,6 +9,7 @@ module.exports = (BaseModel, bookshelf) => BaseModel.extend({
   schema: {
     id: Joi.number().integer(),
     name: Joi.string().required(),
+    url: Joi.string().uri().required(),
     file_size: Joi.number().integer(),
     file_type: Joi.string().allow(null)
   }
