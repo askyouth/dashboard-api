c5818b79c97d8f92294876a03fde9102e30bf17c
David Jelic <david@uxpassion.com>
2017-01-19 16:20:13 +0100

    Update seed files

--- server/database/seeds/init.js
+++ server/database/seeds/init.js
@@ -16,9 +16,13 @@ exports.seed = (knex, Promise) => Promise.all([
   knex('topic').insert([
     topic('Sexual orientation & gender rights'),
     topic('Economic crisis / Economy'),
-    topic('Terrorism'),
     topic('NEETs'),
     topic('Immigration'),
-    topic('Environment')
+    topic('Environment'),
+    topic('Terrorism'),
+    topic('Education & Employment'),
+    topic('EU (dis)integration'),
+    topic('Democracy & human rights'),
+    topic('Health')
   ])
 ]))
